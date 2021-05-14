384e491e556bcdda996f82ecac7de88a6f196aea
Gabriel Maximo <gbielmaximo@gmail.com>
2021-05-14 23:28:35 +0000

    ping pingando

--- src/commands.js
+++ src/commands.js
@@ -1,6 +1,17 @@
 const prefix = "!!"
+const client = require('../index.js')
 
 module.exports = (message) => {
   const content = message.content
+  if (!content.startsWith(prefix) || message.author.bot) return;
+  
+  const args = content.slice(prefix.length).trim().split(' ');
+  const command = args.shift().toLowerCase();
+  
   console.log(content)
+  try {
+    require(`./commands/${command}.js`)(client,message,args)
+  } catch(err) {
+    console.log(err)
+  }
 }
