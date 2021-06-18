8ce7a526d2192e856d11571c0f3b48f42366857e
Defmc <defmc0101@gmail.com>
2021-06-17 21:40:35 -0300

    Adicionado novo argumento de entrada 'configs' para dados estáticos
    Novo argumento 'configs' com a intenção de permitir a interação entre comandos a partir de um dicionário.

--- src/commands.js
+++ src/commands.js
@@ -2,16 +2,19 @@ const prefix = '!!';
 
 const client = require('../index.js')
 
+var configs = {};
+
 module.exports = (message) => {
-  const content = message.content
-  if (!content.startsWith(prefix) || message.author.bot) return;
-  
-  const args = content.slice(prefix.length).trim().split(' ');
-  const command = args.shift().toLowerCase();
-  
-  try {
-    require(`./commands/${command}.js`)({client,message,args})
-  } catch(err) {
-    console.log(err)
-  }
+	const content = message.content;
+	if (!content.startsWith(prefix) || message.author.bot) return;
+	const args = content.slice(prefix.length).trim().split(' ');
+	const command = args.shift().toLowerCase();
+
+	console.log("Executing command '" + command + "'");
+
+	try {
+		require(`./commands/${command}.js`)({client,message,args, configs});
+	} catch(err) {
+		console.log(err);
+  	}
 }
