792f2649fe6fe2d4c94c80b3eb32925a787dc5e3
!Gustavo! <72351688+GustavoGLD@users.noreply.github.com>
2021-06-17 21:19:36 -0300

    Update avatar.js

--- src/commands/avatar.js
+++ src/commands/avatar.js
@@ -1,6 +1,6 @@
 const { MessageAttachment } = require('discord.js');
 
-const avatar = function(message){
+const avatar = function({message}){
     const image = message.author.displayAvatarURL();
     const anexo = new MessageAttachment(image);
     message.channel.send(`aqui seu avatar em anexo:`, anexo);
