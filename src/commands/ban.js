86a66d03a324e017effb7231ad35c9e9061a8fc8
!Gustavo! <72351688+GustavoGLD@users.noreply.github.com>
2021-06-18 17:38:03 -0300

    Update ban.js

--- src/commands/ban.js
+++ src/commands/ban.js
@@ -1,15 +1,33 @@
-const ban = function({message}){
+/**
+ *  Função: Banir um membro
+ * 
+ *  !!ban @<membro> <esclarecer o motivo>
+ */
+
+const ban = function({message, args}){
+
+    //Identificar membro a ser banido
     const user_banned   = message.mentions.users.first();
     const member_banned = message.guild.member(user_banned);
 
+    //Verificar permissão do autor do ban
     if (!message.member.hasPermission('BAN_MEMBERS')){
         message.reply(`tu não pode banir ninguém não, mané`);
         return;
     }
 
+    //Identificar o motivo
+    let the_reason = `(by: ${message.author.tag})`;
+    for (i in args) {
+        if (i != 0){
+            the_reason += " " + args[i];
+        }
+    }  
+
+    //Banir
     member_banned
     .ban({
-        reason: 'Mensagem q vai aparecer nos logs',
+        reason: the_reason,
     })
     .then(() => {
         message.reply(`O membro ${user_banned.tag} foi banido`);
