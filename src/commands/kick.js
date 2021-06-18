3b1d5d6331cca408beb5e9d0a5bbe26e5486eaef
!Gustavo! <72351688+GustavoGLD@users.noreply.github.com>
2021-06-18 17:37:29 -0300

    Update kick.js

--- src/commands/kick.js
+++ src/commands/kick.js
@@ -1,14 +1,32 @@
-const kick = function({message}){
+/** 
+ *  Função: Kickar um membro
+ *
+ *  !!kick @<membro> <esclarecer o motivo>
+ */
+
+const kick = function({message, args}){
+
+    //Identificar membro a ser kickado
     const user_kicked   = message.mentions.users.first();
     const member_kicked = message.guild.member(user_kicked);
 
+    //Verificar permissão do autor do kick
     if (!message.member.hasPermission('KICK_MEMBERS')){
         message.reply(`tu não pode kickar ninguém não, mané`);
         return;
     }
 
+    //Identificar o motivo
+    let the_reason = `(by: ${message.author.tag})`;
+    for (i in args) {
+        if (i != 0){
+            the_reason += " " + args[i];
+        }
+    }   
+
+    //Kickar
     member_kicked
-    .kick('Mensagem q vai aparecer nos logs')
+    .kick(the_reason)
     .then(() => {
         message.reply(`O membro ${user_kicked.tag} foi kickado`);
     })
