8c3b85958b55f5acaa2d490a70ecd715d93d8009
!Gustavo! <72351688+GustavoGLD@users.noreply.github.com>
2021-06-17 21:15:08 -0300

    Create avatar.js

---
+++ src/commands/avatar.js
@@ -0,0 +1,9 @@
+const { MessageAttachment } = require('discord.js');
+
+const avatar = function(message){
+    const image = message.author.displayAvatarURL();
+    const anexo = new MessageAttachment(image);
+    message.channel.send(`aqui seu avatar em anexo:`, anexo);
+}
+
+module.exports = avatar;
