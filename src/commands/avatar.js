4402a27b03e61019e1ef0c493df02ff33eb4d7e6
!Gustavo! <72351688+GustavoGLD@users.noreply.github.com>
2021-06-19 10:21:48 -0300

    Update avatar.js

--- src/commands/avatar.js
+++ src/commands/avatar.js
@@ -2,14 +2,22 @@
  *  Função: exibir foto de perfil de quem solicitar o comando
  *  
  *  !!avatar
+ *  !!avatar @<membro_tag>
  */
 
 const { MessageAttachment } = require('discord.js');
 
-const avatar = function({message}){
-    const image = message.author.displayAvatarURL();
+module.exports = async ({message, args, client}) =>{
+    console.log(message.content);
+
+
+    const user = await message.mentions.users.first() || message.author;
+    console.log(args[0]);
+    console.log(user);
+
+
+    const image = user.displayAvatarURL();
     const anexo = new MessageAttachment(image);
     message.channel.send(`aqui seu avatar em anexo:`, anexo);
-}
 
-module.exports = avatar;
+}
