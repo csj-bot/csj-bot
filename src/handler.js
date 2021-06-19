d8c971a6c78e88945a741853282b0d11ced973a5
BielMaxBR <gbielmaximo@gmail.com>
2021-06-19 14:43:40 -0300

    setup no main

--- src/handler.js
+++ src/handler.js
@@ -1,11 +1,5 @@
 const handler = require('wax-command-handler')
 const { readdirSync } = require('fs')
-const commandConfig = new handler.CommandConfig("!!",
-    true,
-    "Espere **%TIME%** segundos para executar %CMD%",
-    "Voce nao tem a permissao ``%PERM%`` para executar esse comando");
-
-handler.setup(commandConfig);
 
 for (const file of readdirSync(__dirname + "/commands").filter(file => file.endsWith('.js'))) {
     const command = require(`./commands/${file}`);
