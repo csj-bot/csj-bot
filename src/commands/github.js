f38ad94b22efdb74dd7fe9f61ad99a04a8590be8
BielMaxBR <gbielmaximo@gmail.com>
2021-06-19 14:55:33 -0300

    git

---
+++ src/commands/github.js
@@ -0,0 +1,19 @@
+/**
+ *  Função: retornar o repositorio do projeto
+ *  
+ *  !!github
+ *  !!git
+ *  !!gh
+ */
+
+ module.exports = {
+
+    name: 'github',
+    aliases: [ "git", "gh" ],
+    cooldown: 5,
+    description: "retorna o repositorio do projeto",
+    execute({message}) {
+        message.channel.send("https://github.com/csj-bot-csj-bot");
+
+    },
+};
