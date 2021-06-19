5f01a028e20e7b20bec08fccaaebf1e9e6799e36
GustavoGLD <gustavo.relativisco@gmail.com>
2021-06-19 20:15:01 -0300

    commands update

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,5 +1,13 @@
-module.exports = async ({message}) => {
-  const bot_message = await message.channel.send("Ping?");
-  console.log("Ping requested");
-  bot_message.edit(`Pong! A Latência é ${bot_message.createdTimestamp - message.createdTimestamp}ms.`);
-}
+module.exports = {
+  name: "ping",
+  description: "medir latência da conexão do bot",
+  aliases: [ "teste", "pong" ],
+  usage: "!!ping",
+  cooldown: 5,
+  
+  execute(client, message, args) {
+    const bot_message = message.channel.send("Ping?");
+    console.log("Ping requested");
+    bot_message.edit(`Pong! A Latência é ${bot_message.createdTimestamp - message.createdTimestamp}ms.`);
+  },
+};
