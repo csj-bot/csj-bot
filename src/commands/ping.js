f037961e3f207f2b47fe7612f474a1159c97e11c
Gabriel Maximo <gbielmaximo@gmail.com>
2021-06-19 23:10:20 +0000

    pausa pro pc

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,5 +1,15 @@
-module.exports = async ({message}) => {
-  const bot_message = await message.channel.send("Ping?");
-  console.log("Ping requested");
-  bot_message.edit(`Pong! A Latência é ${bot_message.createdTimestamp - message.createdTimestamp}ms.`);
+module.exports = {
+    name: 'ping',
+    aliases:['pg'],
+    cooldown: 5,
+    description: "retorna o ping",
+    async execute(client, message) {
+        const bot_message = await message.channel.send("Ping?");
+        console.log("Ping requested");
+        bot_message.edit(`Pong! A Latência é ${bot_message.createdTimestamp - message.createdTimestamp}ms.`);
+    },
+    slash(client, handler, data) {
+        console.log(data) 
+        handler.postSlashMessage(data, "https://github.com/csj-bot/csj-bot")
+    }
 }
