bc3c07f8ce4447b0c6f88badd78f37bb48a53891
!Gustavo! <72351688+GustavoGLD@users.noreply.github.com>
2021-06-19 14:47:59 -0300

    Update avatar.js

--- src/commands/avatar.js
+++ src/commands/avatar.js
@@ -5,16 +5,21 @@
  *  !!avatar @<membro_tag>
  */
 
-const { MessageAttachment } = require('discord.js');
+ module.exports = {
 
-module.exports = async ({message, args, client}) =>{
-    
-    //Identificando usuário a qual pegar avatar
-    const user = await message.mentions.users.first() || message.author;
-    
-    //Pegar avatar e enviar como anexo
-    const image = user.displayAvatarURL();
-    const anexo = new MessageAttachment(image);
-    message.channel.send(`aqui seu avatar em anexo:`, anexo);
+    name: 'avatar',
+    aliases: [ "foto", "perfil" ],
+    cooldown: 5,
 
-}
+    execute({message}) {
+
+        //Identificando usuário a qual pegar avatar
+        const user = await message.mentions.users.first() || message.author;
+        
+        //Pegar avatar e enviar como anexo
+        const image = user.displayAvatarURL();
+        const anexo = new MessageAttachment(image);
+        message.channel.send(`aqui seu avatar em anexo:`, anexo);
+
+    },
+};
