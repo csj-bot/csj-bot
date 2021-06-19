f304dce02c8dfec22557ee821ac0860a2b4661f3
!Gustavo! <72351688+GustavoGLD@users.noreply.github.com>
2021-06-19 14:53:57 -0300

    Update kick.js

--- src/commands/kick.js
+++ src/commands/kick.js
@@ -1,39 +1,37 @@
 /** 
  *  Função: Kickar um membro
  *
- *  !!kick @<membro> <esclarecer o motivo>
+ *  !!kick @<membro_tag> <esclarecer o motivo>
  */
 
-const kick = function({message, args}){
+ module.exports = {
+    name: 'kick',
+    aliases: [ "expulsar", "expulse", "kickar" ],
+    cooldown: 5,
+    permissions: [ "KICK_MEMBERS" ],
 
-    //Identificar membro a ser kickado
-    const user_kicked   = message.mentions.users.first();
-    const member_kicked = message.guild.member(user_kicked);
+    execute({message}) {    
+        //Identificar membro a ser kickado
+        const user_kicked   = message.mentions.users.first();
+        const member_kicked = message.guild.member(user_kicked);
 
-    //Verificar permissão do autor do kick
-    if (!message.member.hasPermission('KICK_MEMBERS')){
-        message.reply(`tu não pode kickar ninguém não, mané`);
-        return;
-    }
+        //Identificar o motivo
+        let the_reason = `(by: ${message.author.tag})`;
+        for (i in args) {
+            if (i != 0){
+                the_reason += " " + args[i];
+            }
+        }   
 
-    //Identificar o motivo
-    let the_reason = `(by: ${message.author.tag})`;
-    for (i in args) {
-        if (i != 0){
-            the_reason += " " + args[i];
-        }
-    }   
-
-    //Kickar
-    member_kicked
-    .kick(the_reason)
-    .then(() => {
-        message.reply(`O membro ${user_kicked.tag} foi kickado`);
-    })
-    .catch(err => {
-        message.reply(`não foi possível kickar o membro ${user_kicked.tag}`);
-        console.error(err);
-    });
-}
-
-module.exports = kick;
+        //Kickar
+        member_kicked
+        .kick(the_reason)
+        .then(() => {
+            message.reply(`o membro ${user_kicked.tag} foi kickado`);
+        })
+        .catch(err => {
+            message.reply(`não foi possível kickar o membro ${user_kicked.tag}`);
+            console.error(err);
+        });
+    },
+};
