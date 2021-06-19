1196aaa7716adfe8c7e99ba69dcc036318afe77e
BielMaxBR <gbielmaximo@gmail.com>
2021-06-19 15:26:09 -0300

    teste de return

--- src/commands/github.js
+++ src/commands/github.js
@@ -13,7 +13,7 @@
     cooldown: 5,
     description: "retorna o repositorio do projeto",
     execute(obj) {
-        console.log(obj)
+        console.log(Object.keys(obj))
         let message = obj.message
         message.channel.send("https://github.com/csj-bot-csj-bot");
 
