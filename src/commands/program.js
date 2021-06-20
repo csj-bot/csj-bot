32e04605148e719877c23e11d9ce6d1743fb27f5
BielMaxBR <gbielmaximo@gmail.com>
2021-06-20 13:30:35 -0300

    init do program

---
+++ src/commands/program.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "program",
+    aliases: ["programar"],
+    description:"Cria uma mensagem programavel",
+    usage: "!!programar <data> <mensagem>",
+    execute(client,message,args) {
+        
+    }
+}
