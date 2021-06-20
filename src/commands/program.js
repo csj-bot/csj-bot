e3fc41e2b2eadb3167243e5afe18ce87a9fcc4f5
BielMaxBR <gbielmaximo@gmail.com>
2021-06-20 15:29:04 -0300

    pausa pro pc

--- src/commands/program.js
+++ src/commands/program.js
@@ -1,9 +1,29 @@
 module.exports = {
     name: "program",
-    aliases: ["programar"],
-    description:"Cria uma mensagem programavel",
+    aliases: ["programar", "marcar"],
+    description: "Cria uma mensagem programavel",
     usage: "!!programar <data> <mensagem>",
-    execute(client,message,args) {
+    cooldown: 10,
+    execute(client, message, args) {
+        let unformatDate = args[0]
+        let msg = ''
+        for (let i = 1; i < args.length; i++) {
+            msg += args[i]
+        }
+        let date = new Date()
+        let err = convertDate(date, unformatDate)
+        if(err) {
+            message.reply('data invalida')
+            return
+        }
+        message.channel.send(date.getTime())
+    }
+}
+
+function convertDate(date, text) {
+    if(text.length >= 2 && text.length <= 3) {
         
+    } else {
+        return true
     }
 }
