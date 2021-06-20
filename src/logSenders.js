c3975315dab8aab96d96cdd31a14f5e9fcea2028
lllggghhhaaa <lgha1423@outlook.com>
2021-06-20 17:01:53 -0300

    Log support with wax-logger

---
+++ src/logSenders.js
@@ -0,0 +1,5 @@
+const handler = require("wax-logger");
+
+module.exports.kickSender = new handler.LogBase("Kick", 6);
+module.exports.pingSender = new handler.LogBase("Ping", 7);
+module.exports.mainSender = new handler.LogBase("Main", 15);
