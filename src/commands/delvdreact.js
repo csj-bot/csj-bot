129cb44ee4f3d95c46849b35ed33c3054d1e1e8d
GustavoGLD <gustavo.relativisco@gmail.com>
2021-06-21 15:27:17 -0300

    commands update

---
+++ src/commands/delvdreact.js
@@ -0,0 +1,24 @@
+const videoreaction = require("../database/models/vdreact");
+const logger = require("wax-logger");
+const { mainSender } = require("../logSenders");
+
+module.exports = {
+    name: "delvdreaction",
+    description: "Remover uma reação de vídeo",
+    aliases: ["delvdreact", "deletevdreact", "delvdreacts", "deletevdreacts"],
+    usage: "!!delvdreaction <n1>",
+    cooldown: 5,
+    permissions: ["MANAGE_MESSAGES"],
+
+    async execute(client, message, args) {
+        
+        videoreaction.find(async (err, reactions) => {
+            if (err) return logger.logError(mainSender, `err: deletevideoreaction: ${err}`, true);
+
+            for (n in args){
+                await videoreaction.findOneAndDelete({ "reaction" : reactions[args[n]].reaction })
+                await message.channel.send(`VideoReaction "${reactions[args[n]].reaction}" foi deletada`);
+            }
+        });
+    },
+};
