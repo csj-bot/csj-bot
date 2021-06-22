e9e6fe98b96384182090ae2bed8a7f243241fc90
GustavoGLD <gustavo.relativisco@gmail.com>
2021-06-22 11:25:32 -0300

    vdreact.js update

--- src/database/models/vdreact.js
+++ src/database/models/vdreact.js
@@ -7,8 +7,17 @@ const db = require('../connection');
 
 const videoreaction_schema = new db.Schema({
     reaction: String
-})
+});
 
-const videoreaction = db.model('videoreaction', videoreaction_schema)
+ //armaza o número da próxima frase mandar, e o chat a qual mandar
+ const vdr_events_schema = new db.Schema({
+    count: Number,
+    chatID: String
+});
+
+const videoreaction = db.model('videoreaction', videoreaction_schema);
+
+const vdr_events = db.model('vdr_events', vdr_events_schema);
 
 module.exports = videoreaction;
+exports.vdr_events = vdr_events;
